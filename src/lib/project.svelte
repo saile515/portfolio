3630d3bf46e1b5d21b2e60e7405f4f0a66763270
saile515 <elias.jorgensen2006@gmail.com>
2024-02-22 13:04:57 +0100

    add links to projects

--- src/lib/project.svelte
+++ src/lib/project.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-	export let name: string;
-	export let description: string;
-	export let image: string;
+    export let name: string;
+    export let description: string;
+    export let image: string;
+    export let url: string;
 </script>
 
-<div class="flex flex-col w-full max-w-sm bg-mantle rounded-lg overflow-hidden">
-	<img src={image} alt={`Image from ${name} project.`} class="h-80 object-cover object-center" />
-	<div class="p-6">
-		<h3 class="text-2xl">{name}</h3>
-		<p>{description}</p>
-	</div>
-</div>
+<a href={url} target="_blank"
+    ><div class="flex flex-col w-full h-full max-w-sm bg-mantle rounded-lg overflow-hidden">
+        <img src={image} alt={`Image from ${name} project.`} class="h-80 object-cover object-center" />
+        <div class="p-6">
+            <h3 class="text-2xl">{name}</h3>
+            <p>{description}</p>
+        </div>
+    </div>
+</a>
